70337b1e754ae007531091461017cd8316725f14
Its-Vaibhav-2005 <pandey.vaibhav311005@gmail.com>
2023-09-26 09:46:27 +0530

    New Folder and Items By Vaibhav Pandey

---
+++ VaibhavPandey/R-Lang/RFirstCode.R
@@ -0,0 +1,45 @@
+var.int = 5
+var.float = 15.5
+var.string = "VaibhavPandey"
+var.char = "a"
+var.bool.1 = FALSE
+var.bool.2 = F
+var.bool.3 = TRUE
+var.bool.4 = T
+
+x = 5
+y = 15
+
+x*y
+y/x
+x<y
+x>y
+x == y
+x != y
+x == y | x < y
+x == y & x < y
+
+x^2
+x**2
+
+tan(45*(pi/180))
+
+log(10,2)
+
+var = "I am the great Vaibhav:Pandey"
+var.new = sub(":","-",var)
+var.new.two = sub(":","-",var.new)
+var.new
+
+var.ip = "192.161.1.65"
+var.substr = substr(var.ip, start=0, stop=3)
+var.substr
+
+var.string
+nchar(var.string)
+
+
+
+
+
+
